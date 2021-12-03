127b2501d2debaaee08327cfc4692c37cc165e07
Tyson McNulty <mcnultyt@vmware.com>
2021-12-02 19:27:02 -0500

    Add day 2 puzzle 1

--- src/day02.py
+++ src/day02.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def load_steps(steps_file):
     steps = []
     with open(steps_file, "r") as dfile:
@@ -6,3 +8,20 @@ def load_steps(steps_file):
             steps.append([direction, int(amount)])
 
     return steps
+
+def calc_position(steps_file):
+
+    steps = load_steps(steps_file)
+
+    def advance(position, step):
+        direction, amount = step
+        if (direction == "forward"):
+            position["x"] += amount
+        if (direction == "down"):
+            position["y"] += amount
+        if (direction == "up"):
+            position["y"] -= amount
+
+        return position
+
+    return reduce(advance, steps, { "x": 0, "y": 0 })
--- tests/day02_spec.py
+++ tests/day02_spec.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.day02 import load_steps
+from src.day02 import load_steps, calc_position
 
 class DayO1Tests(unittest.TestCase):
     def test_load_steps(self):
@@ -14,3 +14,7 @@ class DayO1Tests(unittest.TestCase):
         ], load_steps('data/day02_steps_test.txt'))
 
         self.assertEqual(1000, len(load_steps('data/day02_steps_tm.txt')))
+
+    def test_calc_position(self):
+        self.assertEqual({ "x": 15, "y": 10 }, calc_position('data/day02_steps_test.txt'))
+        self.assertEqual({ "x": 1971, "y": 830 }, calc_position('data/day02_steps_tm.txt'))
