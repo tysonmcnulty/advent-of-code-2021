4be771bc9760b798be4ccfe8d1c60ed0f89a5bf6
Tyson McNulty <mcnultyt@vmware.com>
2021-12-08 13:37:15 -0500

    Add day 5 solutions

--- src/day05.py
+++ src/day05.py
@@ -1,4 +1,7 @@
+from itertools import chain
 from math import copysign
+from functools import reduce
+from collections import defaultdict
 
 def load_vent_lines(vent_lines_file):
     vent_lines = []
@@ -11,14 +14,44 @@ def load_vent_lines(vent_lines_file):
 class VentLine:
     def __init__(self, vent_line_spec):
         p1, p2 = [coord.strip().split(',') for coord in vent_line_spec.split('->')]
-        x1, y1 = list(map(int, p1))
-        x2, y2 = list(map(int, p2))
-        if x1 == x2:
-            self.vent_points = set([ (x1, y) for y in range(y1, y2, int(copysign(1, y2 - y1))) ]) | set([(x2, y2)])
-        elif y1 == y2:
-            self.vent_points = set([ (x, y1) for x in range(x1, x2, int(copysign(1, x2 - x1))) ]) | set([(x2, y2)])
-        else:
-            self.vent_points = set()
+        self.ends = { tuple(map(int, p1)), tuple(map(int, p2)) }
 
     def __eq__(self, other):
-        return self.vent_points == other.vent_points
+        return self.ends == other.ends
+
+    def get_points(self):
+        x1, y1, x2, y2 = chain.from_iterable(list(self.ends))
+        y_step = 0 if y1 == y2 else int(copysign(1, y2 - y1))
+        x_step = 0 if x1 == x2 else int(copysign(1, x2 - x1))
+        num_points = (abs(x1 - x2) if x_step != 0 else abs(y1 - y2)) + 1
+        return { (x1 + i * x_step, y1 + i * y_step) for i in range(0, num_points) }
+
+    def get_crossings(self, other):
+        return self.get_points() & other.get_points()
+
+    def is_diagonal(self):
+        x1, y1, x2, y2 = chain.from_iterable(list(self.ends))
+        return abs(x1 - x2) == abs(y1 - y2)
+
+    def __str__(self):
+        return f"VentLine{self.get_points()}"
+
+def count_all_crossings(vent_lines, ignore_diagonals = False):
+    filtered_vent_lines = list(filter(lambda v: not v.is_diagonal() if ignore_diagonals else True, vent_lines))
+
+    def update_crossings(crossings_record, vent_line_enum):
+        i, vent_line = vent_line_enum
+        for j in range(i + 1, len(filtered_vent_lines)):
+            crossings = vent_line.get_crossings(filtered_vent_lines[j])
+            for c in crossings:
+                crossings_record[c] += 1
+
+        return crossings_record
+
+    all_crossings_record = reduce(
+        update_crossings,
+        enumerate(filtered_vent_lines),
+        defaultdict(lambda: 0)
+    )
+
+    return len(all_crossings_record.keys())
--- tests/day05_spec.py
+++ tests/day05_spec.py
@@ -1,12 +1,14 @@
 import unittest
+import os
 
-from src.day05 import load_vent_lines, VentLine
+from src.day05 import load_vent_lines, VentLine, count_all_crossings
 
 class Day05Tests(unittest.TestCase):
     def test_vent_line(self):
-        self.assertEqual(set([(0,0), (0,1), (0,2), (0,3)]), VentLine("0,0 -> 0,3").vent_points)
-        self.assertEqual(set([(4,6), (3,6), (2,6), (1,6)]), VentLine("4,6 -> 1,6").vent_points)
-        self.assertEqual(set(), VentLine("1,1 -> 5,5").vent_points)
+        self.assertEqual({(0,0), (0,1), (0,2), (0,3)}, VentLine("0,0 -> 0,3").get_points())
+        self.assertEqual({(4,6), (3,6), (2,6), (1,6)}, VentLine("4,6 -> 1,6").get_points())
+        self.assertEqual({(2,2), (3,3), (4,4), (5,5)}, VentLine("2,2 -> 5,5").get_points())
+        self.assertEqual({(7,2), (6,3), (5,4), (4,5)}, VentLine("7,2 -> 4,5").get_points())
 
     def test_load_vent_lines(self):
         vent_lines = load_vent_lines('data/day05_vent_lines_test.txt')
@@ -24,4 +26,49 @@ class Day05Tests(unittest.TestCase):
                 VentLine("5,5 -> 8,2")
             ],
             vent_lines
+        )
+
+    def test_get_crossings(self):
+        self.assertEqual(
+            {(4,4)},
+            VentLine("4,1 -> 4,7").get_crossings(VentLine("1,4 -> 7,4"))
+        )
+
+        self.assertEqual(
+            {(3,4), (3,5)},
+            VentLine("3,1 -> 3,5").get_crossings(VentLine("3,4 -> 3,8"))
+        )
+
+        self.assertEqual(
+            set(),
+            VentLine("6,2 -> 6,8").get_crossings(VentLine("7,9 -> 2,9"))
+        )
+
+    def test_count_all_crossings(self):
+        self.assertEqual(
+            5,
+            count_all_crossings(
+                load_vent_lines('data/day05_vent_lines_test.txt'),
+                ignore_diagonals = True
+            )
+        )
+
+        self.assertEqual(12,count_all_crossings(load_vent_lines('data/day05_vent_lines_test.txt')))
+
+    @unittest.skipUnless(bool(os.getenv('AOC_RUN_SLOW_TESTS')), 'slow test')
+    def test_count_all_crossings_tm(self):
+        self.assertEqual(
+            5576,
+            count_all_crossings(
+                load_vent_lines('data/day05_vent_lines_tm.txt'),
+                ignore_diagonals = True
+            )
+        )
+
+        self.assertEqual(
+            18144,
+            count_all_crossings(
+                load_vent_lines('data/day05_vent_lines_tm.txt'),
+                ignore_diagonals = False
+            )
         )
