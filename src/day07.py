037cb6e4fc5be1d3009f374069471487dfeddae1
Tyson McNulty <mcnultyt@vmware.com>
2021-12-09 18:03:29 -0500

    Add optimizer classes

--- src/day07.py
+++ src/day07.py
@@ -1,12 +1,29 @@
 from math import ceil
+from abc import ABC, abstractmethod
 
 def load_crab_positions(crab_positions_file):
     with open(crab_positions_file, "r") as file:
         return list(map(int, file.__next__().strip().split(',')))
 
-def get_optimal_alignment(crab_positions):
-    optimal_position = sorted(crab_positions)[ceil(len(crab_positions)/2)]
+class CrabPositionOptimizer(ABC):
+    @abstractmethod
+    def get_fuel_consumption(self, from_position, to_position):
+        pass
 
-    fuel_usage = lambda x: sum(map(lambda n: abs(x - n), crab_positions))
+    @abstractmethod
+    def get_optimum(self, crab_positions):
+        pass
 
-    return (optimal_position, fuel_usage(optimal_position))
+class LinearCrabPositionOptimizer(CrabPositionOptimizer):
+
+    def get_fuel_consumption(self, from_position, to_position):
+        return abs(to_position - from_position)
+
+    def get_optimum(self, crab_positions):
+        optimum = sorted(crab_positions)[ceil(len(crab_positions)/2)]
+        total_fuel_consumption_at_optimum = sum(map(
+            lambda x: self.get_fuel_consumption(x, optimum),
+            crab_positions
+        ))
+
+        return optimum, total_fuel_consumption_at_optimum
--- tests/day07_spec.py
+++ tests/day07_spec.py
@@ -1,11 +1,15 @@
 import unittest
 
-from src.day07 import load_crab_positions, get_optimal_alignment
+from src.day07 import LinearCrabPositionOptimizer, load_crab_positions
 
 class Day07Tests(unittest.TestCase):
     def test_load_crab_positions(self):
         self.assertEqual([16,1,2,0,4,2,7,1,2,14], load_crab_positions('data/day07_crab_positions_test.txt'))
 
-    def test_get_optimal_alignment(self):
-        self.assertEqual((2, 37), get_optimal_alignment(load_crab_positions('data/day07_crab_positions_test.txt')))
-        self.assertEqual((376, 352707), get_optimal_alignment(load_crab_positions('data/day07_crab_positions_tm.txt')))
+    def test_get_optimal_position(self):
+        crab_positions_test = load_crab_positions('data/day07_crab_positions_test.txt')
+        crab_positions_tm = load_crab_positions('data/day07_crab_positions_tm.txt')
+
+        linear_optimizer = LinearCrabPositionOptimizer()
+        self.assertEqual((2, 37), linear_optimizer.get_optimum(crab_positions_test))
+        self.assertEqual((376, 352707), linear_optimizer.get_optimum(crab_positions_tm))
