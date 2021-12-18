d28d969331465e08eca847a8ac1d5f097d253d4e
Tyson McNulty <mcnultyt@vmware.com>
2021-12-18 13:00:27 -0500

    Add day 11 solutions

--- src/day11.py
+++ src/day11.py
@@ -1,14 +1,105 @@
-def load_octopi(octopi_file):
-    with open(octopi_file, "r") as file:
-        octopi = []
+from collections import defaultdict, deque
+from functools import partial
+
+def load_octopus_energy_levels(octopus_energy_levels_file):
+    with open(octopus_energy_levels_file, "r") as file:
+        octopus_energy_levels = []
         for line in file:
-            octopi.append(list(map(int, line.strip())))
+            octopus_energy_levels.append(list(map(int, line.strip())))
 
-        return OctopusGrid(octopi)
+        return octopus_energy_levels
 
-class OctopusGrid:
-    def __init__(self, octopi):
-        self._octopi = octopi
+
+class EventEmitter:
+    def __init__(self):
+        self._listeners = defaultdict(lambda: [])
+
+    def add_listener(self, event_name, listener):
+        self._listeners[event_name].append(listener)
+
+    def emit(self, event_name, event_data = None):
+        for listener in self._listeners[event_name]:
+            listener(event_data)
+
+class OctopusGrid(EventEmitter):
+    def __init__(self, octopus_energy_levels):
+        self._grid = []
+        self._actions = deque()
+        self._len = 0
+        for j, energy_level_row in enumerate(octopus_energy_levels):
+            self._grid.append([])
+            for i, energy_level in enumerate(energy_level_row):
+                octopus = Octopus(energy_level)
+                octopus.add_listener("flash", partial(self.on_flash, position = (i, j)))
+                self._grid[-1].append(octopus)
+                self._len += 1
+
+        super().__init__()
+
+    def __iter__(self):
+        return (o for row in self._grid for o in row)
 
     def __eq__(self, other):
-        return self._octopi == other._octopi
+        return self._grid == other._grid
+
+    def __len__(self):
+        return self._len
+
+    def __repr__(self):
+        row_as_str = lambda r: '[' + ', '.join(str(o.energy_level()) for o in r) + ']'
+        rows_str = ",".join(row_as_str(row) for row in self._grid)
+        return f"OctopusGrid([{rows_str}])"
+
+    def get_neighbor_positions(self, position):
+        in_grid = lambda i, j: (
+            0 <= j and j < len(self._grid) and
+            0 <= i and i < len(self._grid[j])
+        )
+
+        return { (i, j)
+            for i in range(position[0] - 1, position[0] + 2)
+            for j in range(position[1] - 1, position[1] + 2)
+            if in_grid(i, j)
+        } - { position }
+
+    def on_flash(self, _, position):
+        def propagate_flash():
+            for neighbor_position in self.get_neighbor_positions(position):
+                i, j = neighbor_position
+                self._grid[j][i].absorb()
+
+        self._actions.append(propagate_flash)
+
+    def step(self, num_steps = 1):
+        for _ in range(num_steps):
+            self.emit("step")
+            for o in self: self._actions.append(o.charge)
+            while self._actions:
+                self._actions.popleft()()
+
+
+class Octopus(EventEmitter):
+    def __init__(self, energy_level):
+        self._energy_level = energy_level
+        super().__init__()
+
+    def __eq__(self, other):
+        return self._energy_level == other._energy_level
+
+    @property
+    def energy_level(self):
+        return self._energy_level
+
+    def charge(self):
+        if self._energy_level == 9:
+            self.flash()
+        else:
+            self._energy_level += 1
+
+    def absorb(self):
+        if self._energy_level > 0:
+            self.charge()
+
+    def flash(self):
+        self._energy_level = 0
+        self.emit("flash")
--- tests/day11_spec.py
+++ tests/day11_spec.py
@@ -1,29 +1,137 @@
 import unittest
 
-from src.day11 import load_octopi, OctopusGrid
+from src.day11 import load_octopus_energy_levels, OctopusGrid
 
 class Day11Tests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.octopi_test = load_octopi('data/day11_octopi_test.txt')
-        cls.octopi_tm = load_octopi('data/day11_octopi_tm.txt')
+        cls.octopus_energy_levels_test = load_octopus_energy_levels('data/day11_octopus_energy_levels_test.txt')
+        cls.octopus_energy_levels_tm = load_octopus_energy_levels('data/day11_octopus_energy_levels_tm.txt')
 
-    def test_load_octopi(self):
-        self.assertEqual(
-            OctopusGrid(
-                [
-                    [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
-                    [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
-                    [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
-                    [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
-                    [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
-                    [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
-                    [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
-                    [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
-                    [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
-                    [5, 2, 8, 3, 7, 5, 1, 5, 2, 6],
-                ]
-            ),
-            self.octopi_test
+    def test_load_octopus_energy_levels(self):
+        self.assertEqual([
+            [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
+            [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
+            [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
+            [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
+            [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
+            [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
+            [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
+            [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
+            [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
+            [5, 2, 8, 3, 7, 5, 1, 5, 2, 6],
+        ], self.octopus_energy_levels_test)
+
+    def test_step(self):
+        grid = OctopusGrid([
+            [1, 1, 1, 1, 1],
+            [1, 9, 9, 9, 1],
+            [1, 9, 1, 9, 1],
+            [1, 9, 9, 9, 1],
+            [1, 1, 1, 1, 1]
+        ])
+
+        grid.step()
+        self.assertEqual(OctopusGrid([
+            [3, 4, 5, 4, 3],
+            [4, 0, 0, 0, 4],
+            [5, 0, 0, 0, 5],
+            [4, 0, 0, 0, 4],
+            [3, 4, 5, 4, 3],
+        ]), grid)
+
+        grid.step()
+        self.assertEqual(OctopusGrid([
+            [4, 5, 6, 5, 4],
+            [5, 1, 1, 1, 5],
+            [6, 1, 1, 1, 6],
+            [5, 1, 1, 1, 5],
+            [4, 5, 6, 5, 4],
+        ]), grid)
+
+    def test_count_flashes(self):
+        flash_detector = FlashDetector()
+        grid = OctopusGrid(self.octopus_energy_levels_test)
+        grid.add_listener("step", flash_detector.on_step)
+        for o in grid:
+            o.add_listener("flash", flash_detector.increment)
+
+        grid.step()
+        grid_after_step_001 = OctopusGrid(
+            load_octopus_energy_levels('data/day11_octopus_energy_levels_test_step_001.txt')
         )
+        self.assertEqual(grid_after_step_001, grid)
+        self.assertEqual(0, flash_detector.amount)
+
+        grid.step()
+        grid_after_step_002 = OctopusGrid(
+            load_octopus_energy_levels('data/day11_octopus_energy_levels_test_step_002.txt')
+        )
+        self.assertEqual(grid_after_step_002, grid)
+        self.assertEqual(35, flash_detector.amount)
+
+        grid.step()
+        grid_after_step_003 = OctopusGrid(
+            load_octopus_energy_levels('data/day11_octopus_energy_levels_test_step_003.txt')
+        )
+        self.assertEqual(grid_after_step_003, grid)
+        self.assertEqual(80, flash_detector.amount)
+
+        grid.step(7)
+        grid_after_step_010 = OctopusGrid(
+            load_octopus_energy_levels('data/day11_octopus_energy_levels_test_step_010.txt')
+        )
+        self.assertEqual(grid_after_step_010, grid)
+        self.assertEqual(204, flash_detector.amount)
+
+        grid.step(90)
+        grid_after_step_100 = OctopusGrid(
+            load_octopus_energy_levels('data/day11_octopus_energy_levels_test_step_100.txt')
+        )
+        self.assertEqual(grid_after_step_100, grid)
+        self.assertEqual(1656, flash_detector.amount)
+
+    def test_flash_detector_tm(self):
+        flash_detector = FlashDetector()
+        grid_tm = OctopusGrid(self.octopus_energy_levels_tm)
+        grid_tm.add_listener("step", flash_detector.on_step)
+        for o in grid_tm:
+            o.add_listener("flash", flash_detector.increment)
+
+        grid_tm.step(100)
+        grid_tm_after_step_100 = OctopusGrid(
+            load_octopus_energy_levels('data/day11_octopus_energy_levels_tm_step_100.txt')
+        )
+        self.assertEqual(grid_tm_after_step_100, grid_tm)
+        self.assertEqual(1562, flash_detector.amount)
+
+        is_synchronized = lambda: flash_detector.last_step_count == len(grid_tm)
+
+        while not is_synchronized(): grid_tm.step()
+
+        self.assertEqual(268, flash_detector.steps)
+        for o in grid_tm: self.assertEqual(0, o.energy_level)
+
+
+class FlashDetector:
+    def __init__(self):
+        self._flash_counts = []
+
+    def increment(self, *_):
+        self._flash_counts[-1] += 1
+
+    def on_step(self, *_):
+        self._flash_counts.append(0)
+
+    @property
+    def amount(self):
+        return sum(self._flash_counts)
+
+    @property
+    def last_step_count(self):
+        return self._flash_counts[-1]
+
+    @property
+    def steps(self):
+        return len(self._flash_counts)
