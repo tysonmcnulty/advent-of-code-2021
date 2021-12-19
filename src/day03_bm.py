e15c558c96cce2c58aa6d11922914d50d17a794b
bmcnulty <bmc@eml.com>
2021-12-18 22:34:01 -0600

    day 3 solved

--- src/day03_bm.py
+++ src/day03_bm.py
@@ -1,4 +1,5 @@
 # Day 3: Binary Diagnostic
+# refactored to use a string of most common bits, instead of a list
 
 
 def load_report(report_file):
@@ -9,53 +10,59 @@ def load_report(report_file):
     return reports
 
 
-def most_common_bits(reports_list):
+def most_common_bits_str(reports_list):
     bit_counts = [0] * len(reports_list[0])
 
     for line in reports_list:
         for num, rbit in enumerate(line):
             bit_counts[num] += int(rbit)
 
-    most_common_list = [1 if x > (len(reports_list) / 2) else 0 for x in bit_counts]
-    return most_common_list
+    most_common_list = [
+        "1" if x >= (len(reports_list) / 2) else "0" for x in bit_counts
+    ]
+    return "".join(most_common_list)
 
 
-def calc_power(report_file):
+def calc_power_str(report_file):
     """Part 1: Calculate power consumption rating of the submarine
-    as the product of gamma rate and epsilon rate."""
+    as the product of gamma rate and epsilon rate.
+    Gats string instead of list for the most common bits."""
     reports = load_report(report_file)
 
-    gamma_list = most_common_bits(reports)
-    gamma_rate = int("".join(map(str, gamma_list)), 2)  # most common bits
+    gamma_str = most_common_bits_str(reports)
+    gamma_rate = int(gamma_str, base=2)
+    # gamma_rate = int("".join(map(str, gamma_list)), 2)  # most common bits
 
-    epsilon_list = [1 if x == 0 else 0 for x in gamma_list]
-    epsilon_rate = int("".join(map(str, epsilon_list)), 2)  # least common bits
-    # or try int(binary_string, base=2)
+    epsilon_str = "".join(
+        ["1" if x == "0" else "0" for x in gamma_str]
+    )  # inverse of binary
+    epsilon_rate = int(epsilon_str, base=2)
 
     result = gamma_rate * epsilon_rate
     return result
 
 
-def calc_life_support(report_file):
+def calc_life_support_str(report_file):
     """Part 2: Calculate life support rating of the submarine
-    as product of oxygen generator rating and CO2 scrubber rating."""
+    as product of oxygen generator rating and CO2 scrubber rating.
+    Gats string instead of list for the most common bits."""
+
     reports = load_report(report_file)
-    bit_counts = [0] * len(reports[0])
-
-    for line in reports:
-        for num, rbit in enumerate(line):
-            bit_counts[num] += int(rbit)
-
-    gamma_list = [1 if x > (len(reports) / 2) else 0 for x in bit_counts]
-    epsilon_list = [1 if x == 0 else 0 for x in gamma_list]
-
     O2_lines = reports[::]
+    CO2_lines = reports[::]
 
-    most_common_list = most_common_bits(O2_lines)
-    for num in range(len(gamma_list)):
-        # TODO recalculate most/least/equal bit_counts for *each* filtered O2_lines list!
-        if (gamma_list[num] == 1) and (len(O2_lines) > 1):
-            O2_lines = [x for x in O2_lines if x[num] == "1"]
-    O2_rating = int("".join(map(str, O2_lines[0])), 2)
-    return O2_rating
+    for bit_num in range(len(reports[0])):
+        if len(O2_lines) > 1:
+            most_O2_bit = most_common_bits_str(O2_lines)[bit_num]
+            O2_lines = [x for x in O2_lines if x[bit_num] == most_O2_bit]
+        if len(CO2_lines) > 1:
+            most_CO2_bit = most_common_bits_str(CO2_lines)[bit_num]
+            CO2_lines = [x for x in CO2_lines if x[bit_num] != most_CO2_bit]
+        if len(O2_lines) + len(CO2_lines) <= 2:
+            break
+
+    O2_rating = int(O2_lines[0], base=2)
+    CO2_rating = int(CO2_lines[0], base=2)
+    life_support_rating = O2_rating * CO2_rating
+    return life_support_rating
 
--- tests/day03_bm_spec.py
+++ tests/day03_bm_spec.py
@@ -1,6 +1,11 @@
 import unittest
 
-from src.day03_bm import load_report, calc_power, calc_life_support
+from src.day03_bm import (
+    load_report,
+    calc_power_str,
+    calc_life_support_str,
+    most_common_bits_str,
+)
 
 
 class Day03Tests(unittest.TestCase):
@@ -24,10 +29,52 @@ class Day03Tests(unittest.TestCase):
         )
         self.assertEqual(1000, len(load_report("data/day03_report_bm.txt")))
 
-    def test_calc_power(self):
-        self.assertEqual(198, calc_power("data/day03_report_test.txt"))
-        self.assertEqual(2261546, calc_power("data/day03_report_bm.txt"))
+    def test_calc_power_str(self):
+        self.assertEqual(198, calc_power_str("data/day03_report_test.txt"))
+        self.assertEqual(2261546, calc_power_str("data/day03_report_bm.txt"))
 
-    def test_calc_life_support(self):
-        self.assertEqual(198, calc_life_support("data/day03_report_test.txt"))
+    def test_most_common_bits_str(self):
+        self.assertEqual(
+            "10110",
+            most_common_bits_str(
+                [
+                    "00100",
+                    "11110",
+                    "10110",
+                    "10111",
+                    "10101",
+                    "01111",
+                    "00111",
+                    "11100",
+                    "10000",
+                    "11001",
+                    "00010",
+                    "01010",
+                ],
+            ),
+        )  # sample data
+        self.assertEqual(
+            "11111",
+            most_common_bits_str(
+                [
+                    "00100",
+                    "11110",
+                    "10110",
+                    "10111",
+                    "10101",
+                    "01111",
+                    "00111",
+                    "11100",
+                    "10000",
+                    "11001",
+                    "01010",
+                    "01011",
+                ],
+            ),
+        )  # modified sample data; equal counts in positions 1 & 4
+
+    def test_calc_life_support_str(self):
+        self.assertEqual(230, calc_life_support_str("data/day03_report_test.txt"))
+        self.assertEqual(6775520, calc_life_support_str("data/day03_report_bm.txt"))
+        # self.assertEqual(6775520, calc_life_support_str("data/day03_report_tm.txt"))
 
