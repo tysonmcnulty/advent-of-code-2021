383911d630ecbb5a847b5f87bff282b31e24a3f6
Tyson McNulty <mcnultyt@vmware.com>
2021-12-18 19:42:44 -0500

    Add CaveGraph

--- src/day12.py
+++ src/day12.py
@@ -1,3 +1,30 @@
+from collections import defaultdict
+
 def load_cave_connections(cave_connections_file):
     with open(cave_connections_file, "r") as file:
-        return [ tuple(line.strip().split("-")) for line in file ]
+        return [ set(line.strip().split("-")) for line in file ]
+
+class CaveMap:
+    def __init__(self, caves = [], connections = []):
+        self._neighborhoods = defaultdict(lambda: set())
+        for cave in caves:
+            self.add_cave(cave)
+        for conn in connections:
+            self.add_connection(*conn)
+
+    def add_cave(self, cave):
+        self._neighborhoods[cave]
+
+    def add_connection(self, cave1, cave2):
+        self._neighborhoods[cave1].add(cave2)
+        self._neighborhoods[cave2].add(cave1)
+
+    def add_path(self, *caves):
+        if len(caves) == 1:
+            self.add_node(caves[0])
+
+        for i in range(len(caves) - 1):
+            self.add_connection(caves[i], caves[i + 1])
+
+    def __eq__(self, other):
+        return self._neighborhoods == other._neighborhoods
--- tests/day12_spec.py
+++ tests/day12_spec.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.day12 import load_cave_connections
+from src.day12 import load_cave_connections, CaveMap
 
 class Day12Tests(unittest.TestCase):
 
@@ -11,11 +11,25 @@ class Day12Tests(unittest.TestCase):
 
     def test_load_cave_connections(self):
         self.assertEqual([
-            ("start", "A"),
-            ("start", "b"),
-            ("A", "c"),
-            ("A", "b"),
-            ("b", "d"),
-            ("A", "end"),
-            ("b", "end"),
+            { "start", "A" },
+            { "start", "b" },
+            { "A", "c" },
+            { "A", "b" },
+            { "b", "d" },
+            { "A", "end" },
+            { "b", "end" },
         ], self.cave_connections_test)
+
+    def test_cave_graph(self):
+        cave_graph_test = CaveMap(connections = self.cave_connections_test)
+
+        expected_cave_graph = CaveMap()
+        expected_cave_graph.add_cave("start")
+        expected_cave_graph.add_cave("end")
+        expected_cave_graph.add_cave("A")
+        expected_cave_graph.add_connection("start", "b")
+        expected_cave_graph.add_connection("end", "b")
+        expected_cave_graph.add_path("c", "A", "b", "d")
+        expected_cave_graph.add_path("start", "A", "end")
+
+        self.assertEqual(expected_cave_graph, cave_graph_test)
