b80904497529cacfbb27a6cc1aeb442df556ee71
Tyson McNulty <mcnultyt@vmware.com>
2021-12-20 19:43:13 -0500

    Add day 13 solutions

--- src/day13.py
+++ src/day13.py
@@ -1,5 +1,7 @@
 import re
 
+from functools import reduce
+
 def load_instructions(cave_connections_file):
     with open(cave_connections_file, "r") as file:
         dots = set()
@@ -7,9 +9,46 @@ def load_instructions(cave_connections_file):
             if not re.match(r"\d+,\d+", dot_line): break
             dots.add(tuple(map(int, dot_line.strip().split(","))))
 
-        folds = list()
+        fold_specs = list()
         for fold_line in file:
             if fold_line.isspace(): continue
-            folds.append(fold_line.removeprefix("fold along ").strip())
+            fold_specs.append(fold_line.removeprefix("fold along ").strip())
 
-        return (dots, folds)
+        return (dots, fold_specs)
+
+def fold(dots, fold_specs):
+    def fold_once(dots, fold_spec):
+        dimension, position = fold_spec.split("=")
+        if dimension == "x":
+            return fold_right(dots, width(dots) - int(position))
+        if dimension == "y":
+            return fold_down(dots, height(dots) - int(position))
+
+    return rotate_180(reduce(fold_once, fold_specs, rotate_180(dots)))
+
+
+def fold_down(dots, amount):
+    return set(map(lambda d: (d[0], abs(d[1] - amount)), dots))
+
+def fold_right(dots, amount):
+    return set(map(lambda d: (abs(d[0] - amount) - 1, d[1]), dots))
+
+def width(dots):
+    return max(map(lambda d: d[0], dots))
+
+def height(dots):
+    return max(map(lambda d: d[1], dots))
+
+def rotate_180(dots):
+    max_x = width(dots)
+    max_y = height(dots)
+    return set(map(lambda d: (max_x - d[0], max_y - d[1]), dots))
+
+def write_activation_code(dots, dot_symbol = "#", empty_symbol = "."):
+    w = width(dots)
+    h = height(dots)
+    activation_code = [ [empty_symbol] * (w + 1) for _ in range(h + 1) ]
+    for d in dots:
+        activation_code[d[1]][d[0]] = dot_symbol
+
+    return list(map(lambda row: ''.join(row), activation_code))
--- tests/day13_spec.py
+++ tests/day13_spec.py
@@ -2,7 +2,7 @@ import unittest
 
 from collections import Counter
 
-from src.day13 import load_instructions
+from src.day13 import load_instructions, fold, write_activation_code
 
 class Day13Tests(unittest.TestCase):
 
@@ -38,3 +38,44 @@ class Day13Tests(unittest.TestCase):
 
         self.assertEqual(1022, len(self.instructions_tm[0]))
         self.assertEqual(12, len(self.instructions_tm[1]))
+
+    def test_fold_once(self):
+        dots, fold_specs = self.instructions_test
+        self.assertEqual({
+            (0, 0),
+            (2, 0),
+            (3, 0),
+            (6, 0),
+            (9, 0),
+            (0, 1),
+            (4, 1),
+            (6, 2),
+            (10, 2),
+            (0, 3),
+            (4, 3),
+            (1, 4),
+            (3, 4),
+            (6, 4),
+            (8, 4),
+            (9, 4),
+            (10, 4),
+        }, fold(dots, fold_specs[:1]))
+        self.assertEqual(842, len(fold(self.instructions_tm[0], self.instructions_tm[1][:1])))
+
+    def test_write_activation_code(self):
+        self.assertEqual([
+            "#####",
+            "#...#",
+            "#...#",
+            "#...#",
+            "#####",
+        ], write_activation_code(fold(*self.instructions_test)))
+
+        self.assertEqual([
+            "###..####.#..#.###...##....##.####.#..#",
+            "#..#.#....#.#..#..#.#..#....#....#.#..#",
+            "###..###..##...#..#.#.......#...#..#..#",
+            "#..#.#....#.#..###..#.......#..#...#..#",
+            "#..#.#....#.#..#.#..#..#.#..#.#....#..#",
+            "###..#....#..#.#..#..##...##..####..##.",
+        ], write_activation_code(fold(*self.instructions_tm)))
