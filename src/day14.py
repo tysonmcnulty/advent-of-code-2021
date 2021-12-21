705b03edeba5399b5170bfe2547a59bef7b8adb7
Tyson McNulty <mcnultyt@vmware.com>
2021-12-21 08:46:23 -0500

    Add day 14 solutions

--- src/day14.py
+++ src/day14.py
@@ -1,4 +1,6 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
+from functools import reduce
+from math import ceil
 
 def load_instructions(instructions_file):
     with open(instructions_file, "r") as file:
@@ -11,3 +13,50 @@ def load_instructions(instructions_file):
             insertion_rules[pair] = element
 
         return polymer_template, insertion_rules
+
+def grow(polymer_template, insertion_rules, steps = 1):
+    polymer = polymer_template
+    for _ in range(steps):
+        polymer = reduce(
+            lambda p, e: p + insertion_rules[p[-1] + e] + e,
+            polymer[1:],
+            polymer[0]
+        )
+
+    return polymer
+
+class ElementCounter:
+    def __init__(self, polymer_template, insertion_rules):
+        self._pair_counter = Counter([ polymer_template[i:i+2] for i in range(len(polymer_template) - 1) ])
+
+        def update_pair_propagation_rules(pair_propagation_rule, insertion_rule_item):
+            pair, element = insertion_rule_item
+            pair_propagation_rule[pair] = (pair[0] + element, element + pair[1])
+            return pair_propagation_rule
+
+        self._pair_propagation_rules = reduce(
+            update_pair_propagation_rules,
+            insertion_rules.items(),
+            defaultdict(lambda pair: [ pair ])
+        )
+
+    @property
+    def counts(self):
+        element_pair_counter = Counter()
+        for pair, pair_count in self._pair_counter.items():
+            for element in pair:
+                element_pair_counter[element] += pair_count
+
+        for element, count in element_pair_counter.items():
+            element_pair_counter[element] = ceil(count / 2)
+
+        return element_pair_counter
+
+    def grow(self):
+        counter = Counter()
+        for pair, pair_count in self._pair_counter.items():
+            for propagated_pair in self._pair_propagation_rules[pair]:
+                counter[propagated_pair] += pair_count
+
+        self._pair_counter = counter
+
--- tests/day14_spec.py
+++ tests/day14_spec.py
@@ -1,8 +1,9 @@
+from itertools import count
 import unittest
 
 from collections import Counter
 
-from src.day14 import load_instructions
+from src.day14 import load_instructions, grow, ElementCounter
 
 class Day14Tests(unittest.TestCase):
 
@@ -33,3 +34,35 @@ class Day14Tests(unittest.TestCase):
                 "CN": "C",
             }
         ), self.instructions_test)
+
+    def test_grow(self):
+        polymer_template, insertion_rules = self.instructions_test
+        polymer_step_1 = grow(polymer_template, insertion_rules)
+        polymer_step_2 = grow(polymer_step_1, insertion_rules)
+        polymer_step_3 = grow(polymer_step_2, insertion_rules)
+        polymer_step_4 = grow(polymer_step_3, insertion_rules)
+
+        self.assertEqual("NCNBCHB", polymer_step_1)
+        self.assertEqual("NBCCNBBBCBHCB", polymer_step_2)
+        self.assertEqual("NBBBCNCCNBBNBNBBCHBHHBCHB", polymer_step_3)
+        self.assertEqual("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", polymer_step_4)
+
+    def test_grow_tm(self):
+        polymer_template, insertion_rules = self.instructions_tm
+        polymer_tm_step_10 = grow(polymer_template, insertion_rules, steps = 10)
+        element_counts_step_10 = Counter(polymer_tm_step_10)
+        self.assertEqual(3587, max(element_counts_step_10.values()) - min(element_counts_step_10.values()))
+        self.assertEqual(19457, sum(element_counts_step_10.values()))
+
+    def test_pair_counter(self):
+        counter_tm = ElementCounter(*self.instructions_tm)
+
+        for _ in range(10): counter_tm.grow()
+        element_counts_step_10 = counter_tm.counts
+        self.assertEqual(3587, max(element_counts_step_10.values()) - min(element_counts_step_10.values()))
+        self.assertEqual(19457, sum(element_counts_step_10.values()))
+
+        for _ in range(30): counter_tm.grow()
+        element_counts_step_40 = counter_tm.counts
+        self.assertEqual(3906445077999, max(element_counts_step_40.values()) - min(element_counts_step_40.values()))
+        self.assertEqual(20890720927745, sum(element_counts_step_40.values()))
