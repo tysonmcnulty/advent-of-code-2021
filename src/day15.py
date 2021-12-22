d27035ec53f4883ba5d31e41f94845b61464331d
Tyson McNulty <mcnultyt@vmware.com>
2021-12-21 20:52:38 -0500

    Add day 15 puzzle 1

--- src/day15.py
+++ src/day15.py
@@ -1,3 +1,55 @@
+from functools import partial
+from collections import defaultdict
+
 def load_risk_levels(risk_levels_file):
     with open(risk_levels_file, "r") as file:
         return [ list(map(int, line.strip())) for line in file ]
+
+def navigate(cave, start):
+    def get_total_risk_level(min_risks, start, end, exclude_start = False):
+        return min_risks[end] if not exclude_start else min_risks[end] - min_risks[start]
+
+    def get_safest_path(hops_back, position):
+        current_position = position
+        safest_path_back = []
+        while current_position != None:
+            safest_path_back.append(current_position)
+            current_position = hops_back[current_position]
+
+        return list(reversed(safest_path_back))
+
+    hops_back = defaultdict(lambda: None)
+    min_risks = defaultdict(lambda: float("inf"))
+    min_risks[start] = cave.get_risk_level(start)
+    unvisited_positions = { start }
+
+    while unvisited_positions:
+        p = min(unvisited_positions, key = lambda p: min_risks[p])
+        unvisited_positions -= { p }
+
+        for n in cave.get_neighbors(p):
+            risk = min_risks[p] + cave.get_risk_level(n)
+            if risk < min_risks[n]:
+                min_risks[n] = risk
+                hops_back[n] = p
+                unvisited_positions |= { n }
+
+    return partial(get_total_risk_level, min_risks, start), partial(get_safest_path, hops_back)
+
+class Cave:
+    def __init__(self, risk_levels):
+        self._risk_levels = risk_levels
+
+    def get_neighbors(self, position):
+        i, j, = position
+        neighbors = set()
+        if j > 0: neighbors.add((i, j - 1))
+        if i > 0: neighbors.add((i - 1, j))
+        if j < len(self._risk_levels) - 1: neighbors.add((i, j + 1))
+        if i < len(self._risk_levels[j]) - 1: neighbors.add((i + 1, j))
+
+        return neighbors
+
+    def get_risk_level(self, position):
+        i, j = position
+        return self._risk_levels[j][i]
--- tests/day15_spec.py
+++ tests/day15_spec.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.day15 import load_risk_levels
+from src.day15 import load_risk_levels, navigate, Cave
 
 class Day15Tests(unittest.TestCase):
 
@@ -22,3 +22,26 @@ class Day15Tests(unittest.TestCase):
             [1, 2, 9, 3, 1, 3, 8, 5, 2, 1],
             [2, 3, 1, 1, 9, 4, 4, 5, 8, 1],
         ], self.risk_levels_test)
+
+    def test_navigate(self):
+        start = (0, 0)
+        end = (9, 9)
+        get_total_risk_level, get_safest_path = navigate(Cave(self.risk_levels_test), start)
+
+        self.assertEqual(41, get_total_risk_level(end))
+        self.assertEqual(40, get_total_risk_level(end, exclude_start = True))
+        self.assertIn(get_safest_path(end), [
+            [
+                (0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (6, 3),
+                (7, 3), (7, 4), (7, 5), (8, 5), (8, 6), (8, 7), (8, 8), (9, 8), (9, 9),
+            ],[
+                (0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (6, 3),
+                (7, 3), (7, 4), (8, 4), (8, 5), (8, 6), (8, 7), (8, 8), (9, 8), (9, 9),
+            ]])
+
+    def test_navigate_tm(self):
+        start = (0, 0)
+        end = (99, 99)
+        get_total_risk_level, _ = navigate(Cave(self.risk_levels_tm), start)
+
+        self.assertEqual(373, get_total_risk_level(end, exclude_start = True))
