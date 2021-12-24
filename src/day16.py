db17b64de34fa8809284f725dee0c8833b2eaeb3
Tyson McNulty <mcnultyt@vmware.com>
2021-12-23 22:38:46 -0500

    Add day 16 puzzle 2 solution

--- src/day16.py
+++ src/day16.py
@@ -1,4 +1,6 @@
+from enum import Enum
 from itertools import islice
+from functools import reduce
 
 def load_transmission(bits_transmission_file):
     with open(bits_transmission_file, "r") as file:
@@ -15,6 +17,20 @@ def read_bits(bits, n):
         raise EndOfBits()
 
 class Packet:
+    class Type(Enum):
+        SUM = 0
+        PRODUCT = 1
+        MINIMUM = 2
+        MAXIMUM = 3
+        LITERAL = 4
+        GREATER_THAN = 5
+        LESS_THAN = 6
+        EQUAL = 7
+
+    class LengthType(Enum):
+        SUB_PACKET_LENGTH = 0
+        SUB_PACKET_COUNT = 1
+
     def __init__(self, bits, exhaust = True):
         self._bits = ''
         self._suffix = None
@@ -26,9 +42,9 @@ class Packet:
             return self._bits[-n:]
 
         self._version = int(advance_bits(3), 2)
-        self._type_id = int(advance_bits(3), 2)
+        self._type = Packet.Type(int(advance_bits(3), 2))
 
-        if self._type_id == 4:  # literal packet
+        if self._type == Packet.Type.LITERAL:
             literal = []
             while True:
                 block = advance_bits(5)
@@ -38,14 +54,13 @@ class Packet:
             self._value = int(''.join(literal), 2)
 
         else:
-            length_type_id = int(advance_bits(1))
-            if length_type_id == 0:
-
+            self._length_type = Packet.LengthType(int(advance_bits(1)))
+            if self._length_type == Packet.LengthType.SUB_PACKET_LENGTH:
                 sub_packets_length = int(advance_bits(15), 2)
                 self._sub_packets += Packets.from_bits(
                     islice(bits, sub_packets_length),
                     exhaust = False)
-            elif length_type_id == 1:
+            elif self._length_type == Packet.LengthType.SUB_PACKET_COUNT:
                 sub_packets_count = int(advance_bits(11), 2)
                 self._sub_packets += (
                     Packet(bits, exhaust=False) for _ in range(sub_packets_count))
@@ -54,15 +69,26 @@ class Packet:
             self._suffix = read_bits(bits, -len(self.bits) % 8)
             assert not any(b == "1" for b in self._suffix), f"Encountered nonzero padding at end of packet: {self._bits[:-len(self._suffix)]}[{self._suffix}]"
 
-
     @property
     def version(self): return self._version
 
     @property
-    def type_id(self): return self._type_id
+    def type(self): return self._type
 
     @property
-    def value(self): return self._value
+    def value(self):
+        if self._type == Packet.Type.LITERAL:
+            return self._value
+        else:
+            return reduce({
+                Packet.Type.SUM: lambda acc, curr: acc + curr,
+                Packet.Type.PRODUCT: lambda acc, curr: acc * curr,
+                Packet.Type.MINIMUM: lambda acc, curr: min(acc, curr),
+                Packet.Type.MAXIMUM: lambda acc, curr: max(acc, curr),
+                Packet.Type.GREATER_THAN: lambda x1, x2: int(x1 > x2),
+                Packet.Type.LESS_THAN: lambda x1, x2: int(x1 < x2),
+                Packet.Type.EQUAL: lambda x1, x2: int(x1 == x2)
+            }[self._type], map(lambda p: p.value, self._sub_packets))
 
     @property
     def sub_packets(self): return self._sub_packets
--- tests/day16_spec.py
+++ tests/day16_spec.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from src.day16 import load_transmission, Packets
+from src.day16 import load_transmission, Packets, Packet
 
 class Day16Tests(unittest.TestCase):
 
@@ -16,42 +16,42 @@ class Day16Tests(unittest.TestCase):
             "C0015000016115A2E0802F182340",
             "A0016C880162017C3686B18A3D4780"
         ))))
-        cls.packets_tm = Packets.from_transmission(
-            load_transmission('data/day16_bits_transmission_tm.txt'))
+        cls.packets_tm = list(Packets.from_transmission(
+            load_transmission('data/day16_bits_transmission_tm.txt')))
 
     def test_literal_packet(self):
         packet = self.packets_test[0]
 
         self.assertEqual(6, packet.version)
-        self.assertEqual(4, packet.type_id)
+        self.assertEqual(Packet.Type.LITERAL, packet.type)
         self.assertEqual(2021, packet.value)
 
     def test_operator_packet_with_length_type_0(self):
         packet = self.packets_test[1]
 
         self.assertEqual(1, packet.version)
-        self.assertEqual(6, packet.type_id)
-        self.assertEqual(None, packet.value)
+        self.assertEqual(Packet.Type.LESS_THAN, packet.type)
+        self.assertEqual(1, packet.value)
 
         self.assertEqual(2, len(packet.sub_packets))
-        self.assertEqual(4, packet.sub_packets[0].type_id)
+        self.assertEqual(Packet.Type.LITERAL, packet.sub_packets[0].type)
         self.assertEqual(10, packet.sub_packets[0].value)
-        self.assertEqual(4, packet.sub_packets[1].type_id)
+        self.assertEqual(Packet.Type.LITERAL, packet.sub_packets[1].type)
         self.assertEqual(20, packet.sub_packets[1].value)
 
     def test_operator_packet_with_length_type_1(self):
         packet = self.packets_test[2]
 
         self.assertEqual(7, packet.version)
-        self.assertEqual(3, packet.type_id)
-        self.assertEqual(None, packet.value)
+        self.assertEqual(Packet.Type.MAXIMUM, packet.type)
+        self.assertEqual(3, packet.value)
 
         self.assertEqual(3, len(packet.sub_packets))
-        self.assertEqual(4, packet.sub_packets[0].type_id)
+        self.assertEqual(Packet.Type.LITERAL, packet.sub_packets[0].type)
         self.assertEqual(1, packet.sub_packets[0].value)
-        self.assertEqual(4, packet.sub_packets[1].type_id)
+        self.assertEqual(Packet.Type.LITERAL, packet.sub_packets[1].type)
         self.assertEqual(2, packet.sub_packets[1].value)
-        self.assertEqual(4, packet.sub_packets[2].type_id)
+        self.assertEqual(Packet.Type.LITERAL, packet.sub_packets[2].type)
         self.assertEqual(3, packet.sub_packets[2].value)
 
     def test_version_sums(self):
@@ -64,3 +64,26 @@ class Day16Tests(unittest.TestCase):
         self.assertEqual(31, version_sum([self.packets_test[6]]))
 
         self.assertEqual(901, version_sum(self.packets_tm))
+
+    def test_value(self):
+        packets = list(Packets.from_transmission(' '.join((
+            "C200B40A82",
+            "04005AC33890",
+            "880086C3E88112",
+            "CE00C43D881120",
+            "D8005AC2A8F0",
+            "F600BC2D8F",
+            "9C005AC2F8F0",
+            "9C0141080250320F1802104A08",
+        ))))
+        self.assertEqual(3, packets[0].value)
+        self.assertEqual(54, packets[1].value)
+        self.assertEqual(7, packets[2].value)
+        self.assertEqual(9, packets[3].value)
+        self.assertEqual(1, packets[4].value)
+        self.assertEqual(0, packets[5].value)
+        self.assertEqual(0, packets[6].value)
+        self.assertEqual(1, packets[7].value)
+
+        self.assertEqual(1, len(self.packets_tm))
+        self.assertEqual(110434737925, self.packets_tm[0].value)
