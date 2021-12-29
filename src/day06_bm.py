e7de571aaea625c70fab535831b559ab069a4fac
bmcnulty <bmc@eml.com>
2021-12-28 23:52:08 -0600

    solved Day 6 Lanternfish population

---
+++ src/day06_bm.py
@@ -0,0 +1,64 @@
+# Day 6: Lanternfish
+# Each lanternfish creates a new fish every 7 days
+
+# -read initial list of fish ages (one line in a text file)
+# -write population of fish to a history list
+# -each day: (1) decrease ages by 1;
+#   (2) if 0, add a new fish age 8 & rotate 0 to 6;
+#   (3) write population to the history list
+# -data structure: two lists of integers: fish ages & population history
+# -solution to Part 1: population after 80 days
+#  TODO: for big numbers, use a dict of age counts instead of inefficient list!
+# -solution to Part 2: population after 256 days
+
+
+def load_file(age_file):
+    """ age_file has age of each fish at start of simulation, which decrease each day. 
+    At age timer =0, age restores to 6 (0-based count) and a new fish is born with age 8."""
+
+    with open(age_file, "r") as afile:
+        for line in afile:
+            ages = [int(x) for x in line.split(",")]
+    return ages
+
+
+def load_file_part_2(age_file):
+    """For Part 2, switch data structure from list of each fish to a list of the 
+    counts for each age, ranging 0 thru 9. This will handle much larger populations."""
+    with open(age_file, "r") as afile:
+        age_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for line in afile:
+            ages = [int(x) for x in line.split(",")]
+        for age in ages:
+            age_counts[age] += 1
+    return age_counts
+
+
+def age_one_day(ages):
+    ages += [9] * len([x for x in ages if x == 0])  # 9 will decrement to 8
+    ages = [6 if x == 0 else x - 1 for x in ages]
+    return ages
+
+
+def age_one_day_part_2(age_counts):
+    zero_count = age_counts[0]
+    # age_counts = [6 if x == 0 else x - 1 for x in ages] # decrement old fish ages; 0 count to 8
+    del age_counts[0]  # decrement fish ages
+    age_counts.append(zero_count)  # create new fish
+    age_counts[6] += zero_count  # restart old fish
+    # ages += [9] * len([x for x in ages if x == 0])  # create new fish
+    return age_counts
+
+
+def part_1_simulation(ages, days):
+    """Simulate lanternfish population over several days."""
+    for x in range(days):
+        ages = age_one_day(ages)
+    return ages
+
+
+def part_2_simulation(age_counts, days):
+    """Simulate lanternfish population over many days using age count list (Part 2)."""
+    for x in range(days):
+        age_counts = age_one_day_part_2(age_counts)
+    return age_counts
---
+++ tests/day06_bm_spec.py
@@ -0,0 +1,76 @@
+import unittest
+
+from src.day06_bm import (
+    load_file,
+    load_file_part_2,
+    age_one_day,
+    age_one_day_part_2,
+    part_1_simulation,
+    part_2_simulation,
+)
+
+
+class Day05Tests(unittest.TestCase):
+    def test_load_file(self):
+        ages = load_file("data/day06_lanternfish_test.txt")
+        self.assertEqual(5, len(ages))
+        self.assertEqual([3, 4, 3, 1, 2], ages)
+        ages = load_file("data/day06_lanternfish_bm.txt")
+        self.assertEqual(300, len(ages))
+        self.assertEqual([1, 4, 2, 4, 5], ages[:5])
+        self.assertEqual([4, 2, 2, 2, 3], ages[-5:])
+
+    def test_age_one_day(self):
+        ages = [3, 4, 3, 1, 2]  # the test data
+        self.assertEqual([2, 3, 2, 0, 1], age_one_day(ages))
+        self.assertEqual([1, 2, 1, 6, 0, 8], age_one_day([2, 3, 2, 0, 1]))
+        self.assertEqual([0, 1, 0, 5, 6, 7, 8], age_one_day([1, 2, 1, 6, 0, 8]))
+
+    def test_part_1__simulation(self):
+        ages = [3, 4, 3, 1, 2]  # the test data
+        self.assertEqual(26, len(part_1_simulation(ages, 18)))
+        self.assertEqual(5934, len(part_1_simulation(ages, 80)))
+        ages_bm = load_file("data/day06_lanternfish_bm.txt")
+        self.assertEqual(349549, len(part_1_simulation(ages_bm, 80)))
+        # # Part 2: 256 days ...list of fish too big
+
+    def test_load_file_part_2(self):
+        age_counts = load_file_part_2("data/day06_lanternfish_test.txt")
+        # list of test data is [3, 4, 3, 1, 2]
+        self.assertEqual(9, len(age_counts))
+        self.assertEqual(5, sum(age_counts))
+        self.assertEqual(
+            [0, 1, 1, 2, 1, 0, 0, 0, 0], age_counts,
+        )
+        age_counts = load_file_part_2("data/day06_lanternfish_bm.txt")
+        self.assertEqual(300, sum(age_counts))
+        self.assertEqual(
+            [0, 80, 53, 51, 63, 53, 0, 0, 0], age_counts,
+        )
+
+    def test_age_one_day_part_2(self):
+        age_counts = [0, 1, 1, 2, 1, 0, 0, 0, 0]  # the test data
+        self.assertEqual(
+            [1, 1, 2, 1, 0, 0, 0, 0, 0], age_one_day_part_2(age_counts),
+        )
+        self.assertEqual(
+            [1, 2, 1, 0, 0, 0, 1, 0, 1],
+            age_one_day_part_2([1, 1, 2, 1, 0, 0, 0, 0, 0]),
+        )
+        self.assertEqual(
+            [2, 1, 0, 0, 0, 1, 1, 1, 1],
+            age_one_day_part_2([1, 2, 1, 0, 0, 0, 1, 0, 1]),
+        )
+
+    def test_part_2__simulation(self):
+        age_counts = [0, 1, 1, 2, 1, 0, 0, 0, 0]  # the test data
+        self.assertEqual(26, sum(part_2_simulation(age_counts, 18)))
+        age_counts = [0, 1, 1, 2, 1, 0, 0, 0, 0]  # the test data
+        self.assertEqual(5934, sum(part_2_simulation(age_counts, 80)))
+        age_counts_bm = load_file_part_2("data/day06_lanternfish_bm.txt")
+        self.assertEqual(349549, sum(part_2_simulation(age_counts_bm, 80)))
+        # Part 2: 256 days ...big numbers
+        age_counts = [0, 1, 1, 2, 1, 0, 0, 0, 0]  # the test data
+        self.assertEqual(26984457539, sum(part_2_simulation(age_counts, 256)))
+        age_counts_bm = load_file_part_2("data/day06_lanternfish_bm.txt")
+        self.assertEqual(1589590444365, sum(part_2_simulation(age_counts_bm, 256)))
