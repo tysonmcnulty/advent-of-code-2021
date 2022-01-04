3ab7c63b371cd34ee4f9d93821ee37b14fd1e5ca
Tyson McNulty <mcnultyt@vmware.com>
2022-01-04 13:28:22 -0500

    Add day 18 solutions

--- src/day18.py
+++ src/day18.py
@@ -1,4 +1,5 @@
 from ast import literal_eval
+from math import floor, ceil
 
 def load_snailfish_numbers(snailfish_numbers_file):
     with open(snailfish_numbers_file, "r") as file:
@@ -19,35 +20,102 @@ class SnailfishNumber:
 
         assert len(self._pair) == 2, f"Invalid pair: {self._pair}"
 
+    @staticmethod
+    def from_traversal(traversal):
+        left = []
+        right = []
+        for item in traversal:
+            if item[1] == tuple():
+                return item[0]
+            if item[1] < (1,):
+                left.append((item[0], item[1][1:]))
+            else:
+                right.append((item[0], item[1][1:]))
+
+        return SnailfishNumber([
+            SnailfishNumber.from_traversal(left),
+            SnailfishNumber.from_traversal(right)
+        ])
+
     @property
     def magnitude(self):
         left_magnitude, right_magnitude = tuple(map(
-            lambda n: n if isinstance(int, n) else n.magnitude, self._pair))
+            lambda n: n if isinstance(n, int) else n.magnitude, self._pair))
 
         return 3 * left_magnitude + 2 * right_magnitude
 
     @property
     def pair(self): return self._pair
 
-    def reduce(self):
-        return self
+    def reduced(self):
+        i = 0
+        traversal = self.traverse()
+
+        def explode():
+            nonlocal i
+            nonlocal traversal
+            if i - 1 >= 0:
+                traversal[i - 1] = (traversal[i - 1][0] + traversal[i][0], traversal[i - 1][1])
+
+            if i + 2 < len(traversal):
+                traversal[i + 2] = (traversal[i + 1][0] + traversal[i + 2][0], traversal[i + 2][1])
+
+            traversal[i] = (0, traversal[i][1][:-1])
+            del traversal[i + 1]
+
+        def split():
+            nonlocal i
+            nonlocal traversal
+            left = (floor(traversal[i][0] / 2), traversal[i][1] + (0,))
+            right = (ceil(traversal[i][0] / 2), traversal[i][1] + (1,))
+            traversal[i] = left
+            traversal.insert(i + 1, right)
+
+        while i < len(traversal):
+            if len(traversal[i][1]) > 4: explode()
+            i += 1
+
+        i = 0
+        while i < len(traversal):
+            if len(traversal[i][1]) > 4:
+                explode()
+                i = max(i - 1, 0)
+            elif traversal[i][0] >= 10:
+                split()
+            else:
+                i += 1
+
+        return SnailfishNumber.from_traversal(traversal)
+
+    def traverse(self, depth = tuple()):
+        traversal = []
+        for i in (0, 1):
+            if isinstance(self._pair[i], int):
+                traversal.append((self._pair[i], depth + (i,)))
+            else:
+                traversal += self._pair[i].traverse(depth + (i,))
+
+        return traversal
+
 
     def __add__(self, other):
-        return SnailfishNumber([self, other])
+        return SnailfishNumber([self, other]).reduced()
 
     def __radd__(self, other):
-        return SnailfishNumber([other, self])
+        return SnailfishNumber([other, self]).reduced()
 
     def __eq__(self, other):
-        return self.pair == other.pair
+        try:
+            return self.pair == other.pair
+        except:
+            return False
 
     def __str__(self):
-        return str(self.pair)
+        return f"[{str(self.pair[0])}, {str(self.pair[1])}]"
 
     def __repr__(self):
         return ''.join([
             "SnailfishNumber([",
-            f"{str(self.pair[0])}, ",
-            str(self.pair[1]),
+            str(self),
             "])"
         ])
--- tests/day18_spec.py
+++ tests/day18_spec.py
@@ -1,6 +1,11 @@
+import os
 import unittest
 
+from functools import reduce
+
 from src.day18 import load_snailfish_numbers, SnailfishNumber
+
+SN = SnailfishNumber
 
 class Day18Tests(unittest.TestCase):
 
@@ -10,8 +15,106 @@ class Day18Tests(unittest.TestCase):
         cls.snailfish_numbers_tm = load_snailfish_numbers('data/day18_snailfish_numbers_tm.txt')
 
     def test_snailfish_number_add(self):
-        SN = SnailfishNumber
         self.assertEqual(SN([1, [2, 3]]), 1 + SN([2, 3]))
+        self.assertEqual(SN([[1, 2], 3]), SN([1, 2]) + 3)
+        self.assertEqual(
+            SN([[[1, 2], [3, 4]], [5, 6]]),
+            reduce(
+                lambda x, y: x + y,
+                [SN([1, 2]), SN([3, 4]), SN([5, 6])]))
 
-    # def test_homework(self):
-    #     self.assertEqual(4140, sum(self.snailfish_numbers_test).magnitude)
+    def test_traverse(self):
+        number = SN([[[1, 2], [3, 4]], [5, 6]])
+        traversal = [
+            (1, (0, 0, 0)),
+            (2, (0, 0, 1)),
+            (3, (0, 1, 0)),
+            (4, (0, 1, 1)),
+            (5, (1, 0)),
+            (6, (1, 1))
+        ]
+
+        self.assertEqual(traversal, number.traverse())
+        self.assertEqual(number, SN.from_traversal(traversal))
+
+    def test_snailfish_number_reduce(self):
+        self.assertEqual(
+            SN([[[[0,9],2],3],4]),
+            SN([[[[[9,8],1],2],3],4]).reduced()
+        )
+        self.assertEqual(
+            SN([7,[6,[5,[7,0]]]]),
+            SN([7,[6,[5,[4,[3,2]]]]]).reduced()
+        )
+        self.assertEqual(
+            SN([[6,[5,[7,0]]],3]),
+            SN([[6,[5,[4,[3,2]]]],1]).reduced()
+        )
+
+    def test_add(self):
+        self.assertEqual(
+            SN([[[[0,7],4],[[7,8],[6,0]]],[8,1]]),
+            SN([[[[4,3],4],4],[7,[[8,4],9]]]) + SN([1,1])
+        )
+
+        self.assertEqual(
+            SN([[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]),
+            SN([[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]) + SN([7,[[[3,7],[4,3]],[[6,3],[8,8]]]])
+        )
+
+    def test_load_snailfish_numbers(self):
+        self.assertEqual([
+            SN([[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]),
+            SN([[[5,[2,8]],4],[5,[[9,9],0]]]),
+            SN([6,[[[6,2],[5,6]],[[7,6],[4,7]]]]),
+            SN([[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]),
+            SN([[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]),
+            SN([[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]),
+            SN([[[[5,4],[7,7]],8],[[8,3],8]]),
+            SN([[9,3],[[9,9],[6,[4,9]]]]),
+            SN([[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]),
+            SN([[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]),
+        ], self.snailfish_numbers_test)
+
+    def test_magnitude(self):
+        self.assertEqual([
+            143,
+            1384,
+            445,
+            791,
+            1137,
+            3488,
+        ], list(map(lambda it: it.magnitude, [
+            SN([[1,2],[[3,4],5]]),
+            SN([[[[0,7],4],[[7,8],[6,0]]],[8,1]]),
+            SN([[[[1,1],[2,2]],[3,3]],[4,4]]),
+            SN([[[[3,0],[5,3]],[4,4]],[5,5]]),
+            SN([[[[5,0],[7,4]],[5,5]],[6,6]]),
+            SN([[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]),
+        ])))
+
+    def test_add_homework(self):
+        expected_sum = SN([[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]])
+        self.assertEqual(expected_sum, reduce(lambda a, b: a + b, self.snailfish_numbers_test))
+        self.assertEqual(4140, expected_sum.magnitude)
+
+    def test_add_homework_tm(self):
+        self.assertEqual(3892, reduce(lambda a, b: a + b, self.snailfish_numbers_tm).magnitude)
+
+    def test_largest_sum(self):
+        sums = ((a, b, a + b) for a in self.snailfish_numbers_test for b in self.snailfish_numbers_test if a != b)
+        max_sum = max(sums, key = lambda it: it[2].magnitude)
+
+        self.assertEqual((
+            SN([[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]),
+            SN([[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]),
+            SN([[[[7,8],[6,6]],[[6,0],[7,7]]],[[[7,8],[8,8]],[[7,9],[0,6]]]]),
+        ), max_sum)
+
+        self.assertEqual(3993, max_sum[2].magnitude)
+
+    @unittest.skipUnless(bool(os.getenv('AOC_RUN_SLOW_TESTS')), 'slow test')
+    def test_largest_sum_tm(self):
+        sums = ((a, b, a + b) for a in self.snailfish_numbers_tm for b in self.snailfish_numbers_tm if a != b)
+        max_sum = max(sums, key = lambda it: it[2].magnitude)
+        self.assertEqual(3993, max_sum[2].magnitude)
