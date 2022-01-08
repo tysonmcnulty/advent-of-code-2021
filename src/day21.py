cd9e4227cffd0101040549283bd7671908948a45
Tyson McNulty <mcnultyt@vmware.com>
2022-01-08 09:45:34 -0500

    Add load starting spaces test

--- src/day21.py
+++ src/day21.py
@@ -1,5 +1,7 @@
 import re
 
-def load_starting_squares(starting_squares_file):
+def load_starting_spaces(starting_squares_file):
     with open(starting_squares_file, "r") as file:
-        pass
+        matches = (re.match(r"(Player \d+) starting position: (\d+)", line).groups() for line in file)
+
+        return dict(map(lambda m: (m[0], int(m[1])), matches))
--- tests/day21_spec.py
+++ tests/day21_spec.py
@@ -1,11 +1,15 @@
 import os
 import unittest
 
-from src.day21 import load_starting_squares
+from src.day21 import load_starting_spaces
 
 class Day21Tests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.starting_squares_test = load_starting_squares('data/day21_starting_squares_test.txt')
-        cls.starting_squares_tm = load_starting_squares('data/day21_starting_squares_tm.txt')
+        cls.starting_spaces_test = load_starting_spaces('data/day21_starting_spaces_test.txt')
+        cls.starting_spaces_tm = load_starting_spaces('data/day21_starting_spaces_tm.txt')
+
+    def test_load_starting_spaces(self):
+        self.assertEqual({ "Player 1": 4, "Player 2": 8 }, self.starting_spaces_test)
+        self.assertEqual({ "Player 1": 7, "Player 2": 2 }, self.starting_spaces_tm)
