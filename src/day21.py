b91d80b80921bb1efa4c604da85d10ada0b185d7
Tyson McNulty <mcnultyt@vmware.com>
2022-01-08 21:16:44 -0500

    Add puzzle 1 solution

--- src/day21.py
+++ src/day21.py
@@ -1,7 +1,56 @@
 import re
 
-def load_starting_spaces(starting_squares_file):
+def load_pawns(starting_squares_file):
     with open(starting_squares_file, "r") as file:
-        matches = (re.match(r"(Player \d+) starting position: (\d+)", line).groups() for line in file)
+        return tuple(int(re.match(r"Player \d+ starting position: (\d+)", line).groups()[0]) for line in file)
 
-        return dict(map(lambda m: (m[0], int(m[1])), matches))
+class DiracDiceGame:
+    def __init__(self, pawns = (0, 0), score = (0, 0), player_index = 0):
+        self._pawns = pawns
+        self._score = score
+        self._player_index = player_index
+
+    @property
+    def state(self): return (self.pawns, self.score, self.player_index)
+
+    @property
+    def pawns(self): return self._pawns
+
+    @property
+    def score(self): return self._score
+
+    @property
+    def player_index(self): return self._player_index
+
+    def __repr__(self):
+        return f"DiracDiceGame({self.state})"
+
+    def __eq__(self, other):
+        return self.state == other.state
+
+    def play_turn(self, roll):
+        next_pawns_list = list(self.pawns)
+        next_pawns_list[self.player_index] = (next_pawns_list[self.player_index] + roll - 1) % 10 + 1
+        next_pawns = tuple(next_pawns_list)
+
+        next_score_list = list(self.score)
+        next_score_list[self.player_index] += next_pawns[self.player_index]
+        next_score = tuple(next_score_list)
+
+        next_player_index = (self.player_index + 1) % 2
+
+        return DiracDiceGame(next_pawns, next_score, next_player_index)
+
+class DeterministicDie:
+    def __init__(self):
+        self._num_rolls = 0
+        self._value = 100
+
+    def roll(self):
+        self._value = self._value % 100 + 1
+        self._num_rolls += 1
+        return self._value
+
+    @property
+    def num_rolls(self):
+        return self._num_rolls
--- tests/day21_spec.py
+++ tests/day21_spec.py
@@ -1,15 +1,63 @@
-import os
 import unittest
 
-from src.day21 import load_starting_spaces
+from functools import partial
+
+from src.day21 import load_pawns, DiracDiceGame, DeterministicDie
 
 class Day21Tests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.starting_spaces_test = load_starting_spaces('data/day21_starting_spaces_test.txt')
-        cls.starting_spaces_tm = load_starting_spaces('data/day21_starting_spaces_tm.txt')
+        cls.pawns_test = load_pawns('data/day21_pawns_test.txt')
+        cls.pawns_tm = load_pawns('data/day21_pawns_tm.txt')
 
-    def test_load_starting_spaces(self):
-        self.assertEqual({ "Player 1": 4, "Player 2": 8 }, self.starting_spaces_test)
-        self.assertEqual({ "Player 1": 7, "Player 2": 2 }, self.starting_spaces_tm)
+    def test_load_pawns(self):
+        self.assertEqual((4, 8), self.pawns_test)
+        self.assertEqual((7, 2), self.pawns_tm)
+
+    def test_deterministic_game(self):
+        die = DeterministicDie()
+        def roll_three():
+            nonlocal die
+            return sum(die.roll() for _ in range(3))
+
+        game = DiracDiceGame(self.pawns_test)
+
+        self.assertEqual(((4, 8), (0, 0), 0), game.state)
+        self.assertEqual(0, die.num_rolls)
+
+        game = game.play_turn(roll_three())
+        self.assertEqual(((10, 8), (10, 0), 1), game.state)
+        self.assertEqual(3, die.num_rolls)
+
+        game = game.play_turn(roll_three())
+        self.assertEqual(((10, 3), (10, 3), 0), game.state)
+        self.assertEqual(6, die.num_rolls)
+
+        game = game.play_turn(roll_three())
+        self.assertEqual(((4, 3), (14, 3), 1), game.state)
+        self.assertEqual(9, die.num_rolls)
+
+        game = game.play_turn(roll_three())
+        self.assertEqual(((4, 6), (14, 9), 0), game.state)
+        self.assertEqual(12, die.num_rolls)
+
+        while not any(map(lambda score: score >= 1000, game.state[1])):
+            game = game.play_turn(roll_three())
+
+        self.assertEqual(((10, 3), (1000, 745), 1), game.state)
+        self.assertEqual(993, die.num_rolls)
+
+    def test_deterministic_game_tm(self):
+        die = DeterministicDie()
+        def roll_three():
+            nonlocal die
+            return sum(die.roll() for _ in range(3))
+
+        game = DiracDiceGame(self.pawns_tm)
+
+        while not any(map(lambda score: score >= 1000, game.state[1])):
+            game = game.play_turn(roll_three())
+
+        self.assertEqual((1008, 788), game.score)
+        self.assertEqual(861, die.num_rolls)
