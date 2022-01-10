1cf2ad2dafc11d06591a32ad5d009167e36c17d5
Tyson McNulty <mcnultyt@vmware.com>
2022-01-09 22:30:28 -0500

    Add day 23 utilities

---
+++ src/day23.py
@@ -0,0 +1,58 @@
+import re
+from collections import defaultdict
+
+def load_amphipod_diagram(amphipod_diagram_file):
+    amphipods = defaultdict(lambda: set())
+    with open(amphipod_diagram_file, "r") as file:
+        for line_number, line in enumerate(file):
+            for column_number, char in enumerate(line):
+                match = re.match(r"[ABCD]", char)
+                if match:
+                    amphipods[char] |= { (column_number, line_number) }
+
+    return amphipods
+
+class AmphipodGraph:
+    def __init__(
+        self,
+        nodes = set(),
+        edges = {
+            frozenset({(1, 1), (2, 1)}),
+            frozenset({(2, 1), (4, 1)}),
+            frozenset({(2, 1), (3, 2)}),
+            frozenset({(3, 2), (3, 3)}),
+            frozenset({(3, 2), (4, 1)}),
+            frozenset({(4, 1), (6, 1)}),
+            frozenset({(4, 1), (5, 2)}),
+            frozenset({(5, 2), (5, 3)}),
+            frozenset({(5, 2), (6, 1)}),
+            frozenset({(6, 1), (8, 1)}),
+            frozenset({(6, 1), (7, 2)}),
+            frozenset({(7, 2), (7, 3)}),
+            frozenset({(7, 2), (8, 1)}),
+            frozenset({(8, 1), (10, 1)}),
+            frozenset({(8, 1), (9, 2)}),
+            frozenset({(9, 2), (9, 3)}),
+            frozenset({(9, 2), (10, 1)}),
+            frozenset({(10, 1), (11, 1)}),
+        }
+    ):
+        self._adjacencies = defaultdict(lambda: set())
+        for n in nodes: self.add_node(n)
+        for e in edges: self.add_edge(e)
+
+    def add_node(self, node):
+        self._adjacencies[node]
+
+    def add_edge(self, edge):
+        n1, n2 = edge
+        self._adjacencies[n1].add(n2)
+        self._adjacencies[n2].add(n1)
+
+    @property
+    def nodes(self):
+        return self._adjacencies.keys()
+
+    @property
+    def edges(self):
+        return { frozenset({n1, n2}) for n1, n1_neighbors in self._adjacencies.items() for n2 in n1_neighbors }
---
+++ tests/day23_spec.py
@@ -0,0 +1,61 @@
+import os
+import unittest
+
+from src.day23 import load_amphipod_diagram, AmphipodGraph
+
+class Day23Tests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.amphipods_test = load_amphipod_diagram('data/day23_amphipod_diagram_test.txt')
+        cls.amphipods_tm = load_amphipod_diagram('data/day23_amphipod_diagram_tm.txt')
+        cls.amphipods_target = {
+            "A": {(3, 2), (3, 3)},
+            "B": {(5, 2), (5, 3)},
+            "C": {(7, 2), (7, 3)},
+            "D": {(9, 2), (9, 3)}
+        }
+
+    def test_load_amphipod_diagram(self):
+        self.assertEqual({
+            "A": {(3, 3), (9, 3)},
+            "B": {(3, 2), (7, 2)},
+            "C": {(5, 2), (7, 3)},
+            "D": {(5, 3), (9, 2)}
+        }, dict(self.amphipods_test))
+
+        self.assertEqual({
+            "A": {(7, 2), (7, 3)},
+            "B": {(5, 2), (9, 3)},
+            "C": {(3, 2), (3, 3)},
+            "D": {(5, 3), (9, 2)}
+        }, dict(self.amphipods_tm))
+
+    def test_amphipod_graph(self):
+        amphipod_graph = AmphipodGraph()
+        self.assertEqual({
+            (1, 1), (2, 1), (4, 1), (6, 1), (8, 1), (10, 1), (11, 1),
+            (3, 2), (5, 2), (7, 2), (9, 2),
+            (3, 3), (5, 3), (7, 3), (9, 3)
+        }, amphipod_graph.nodes)
+
+        self.assertEqual({
+            frozenset({(1, 1), (2, 1)}),
+            frozenset({(2, 1), (4, 1)}),
+            frozenset({(2, 1), (3, 2)}),
+            frozenset({(3, 2), (3, 3)}),
+            frozenset({(3, 2), (4, 1)}),
+            frozenset({(4, 1), (6, 1)}),
+            frozenset({(4, 1), (5, 2)}),
+            frozenset({(5, 2), (5, 3)}),
+            frozenset({(5, 2), (6, 1)}),
+            frozenset({(6, 1), (8, 1)}),
+            frozenset({(6, 1), (7, 2)}),
+            frozenset({(7, 2), (7, 3)}),
+            frozenset({(7, 2), (8, 1)}),
+            frozenset({(8, 1), (10, 1)}),
+            frozenset({(8, 1), (9, 2)}),
+            frozenset({(9, 2), (9, 3)}),
+            frozenset({(9, 2), (10, 1)}),
+            frozenset({(10, 1), (11, 1)}),
+        }, amphipod_graph.edges)
